d937eb1a28d7be4680762e55cf571509efb44326
matthieu.plessier <plessier.matthieu@gmail.com>
2021-06-14 15:50:28 +0200

    maj

---
+++ exercice2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php 
+            $lastname="plessier"; 
+            $firstname="matthieu"; 
+            $age=43; 
+            // echo "$lastname $firstname $age"
+            echo "Bonjour je m'appelle $lastname $firstname et j'ai $age."
+            // Utiliser la simple cote en priorité. Double cote qd il ya une/des variables(s)
+        ?>
+    </p>
+</body>
+</html>
---
+++ exercice4.php
@@ -0,0 +1,25 @@
+<?php
+    $firstname='Matthieu';
+    $age=43;
+    $height=1.90;
+    $present=true;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "$firstname<br>";
+        echo "$age<br>";
+        echo "$height<br>";
+        echo "$present<br>";
+        // var_dump permet d'afficher la valeur et le type
+    ?>
+
+</body>
+</html>
